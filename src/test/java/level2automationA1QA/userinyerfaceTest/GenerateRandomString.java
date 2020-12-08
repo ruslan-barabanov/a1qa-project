9f5d33f1ebf4489b24036ecf827afe1d1c1723e9
ruslan-barabanov <rem148esenin@gmail.com>
2020-12-08 23:24:43 +0300

    add Task UserInYerFaceTest

---
+++ src/test/java/level2automationA1QA/userinyerfaceTest/GenerateRandomString.java
@@ -0,0 +1,19 @@
+package level2automationA1QA.userinyerfaceTest;
+
+import java.util.Random;
+
+public class GenerateRandomString {
+
+    public String randomString() {
+        int length = 16;
+        Random r = new Random();
+        String s = r.ints(48, 122)
+                .filter(i -> (i < 57 || i > 65) && (i < 90 || i > 97))
+                .mapToObj(i -> (char) i)
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
+        return s;
+    }
+
+}
---
+++ src/test/java/level2automationA1QA/userinyerfaceTest/pages/ThreePageUserInYerFace.java
@@ -0,0 +1,42 @@
+package level2automationA1QA.userinyerfaceTest.pages;
+
+import aquality.selenium.browser.AqualityServices;
+import aquality.selenium.elements.interfaces.ICheckBox;
+import aquality.selenium.elements.interfaces.ILink;
+import aquality.selenium.forms.Form;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Random;
+
+public class ThreePageUserInYerFace extends Form {
+    /**
+     * Constructor with parameters
+     *
+     * @param locator
+     * @param name
+     */
+    public ThreePageUserInYerFace(By locator, String name) {
+        super(locator, name);
+    }
+
+    private final ICheckBox unselectAll = getElementFactory().getCheckBox(By.xpath("//body/div[@id='app']/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[3]/div[1]/div[21]/div[1]/span[1]/label[1]/span[1]/span[1]"), "unselect all");
+    private final ILink photoUpload = getElementFactory().getLink(By.xpath("//a[@class='avatar-and-interests__upload-button']"), "photo upload");
+
+    public ICheckBox clickCheck() {
+        return unselectAll;
+    }
+
+    public ILink getPhotoUpload() {
+        return photoUpload;
+    }
+
+    public void get3Interests() {
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            AqualityServices.getBrowser().getDriver().findElement(By.xpath("/html/body/div/div/div[2]/div[4]/div/div[1]/div/div[3]/div/div[" + random.nextInt(18) + "]/div/span[1]/label/span")).click();
+        }
+    }
+}
+
